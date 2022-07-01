318b1f8b45bf17a1726eb46474c038290affdf3b
Gunjan <GS-0801@GSLAB.COM>
2022-07-01 17:31:05 +0530

    gunjan project

---
+++ src/main/java/com/gunjanproject/example/Projectshowcase/ProjectShowcaseApplication.java
@@ -0,0 +1,13 @@
+package com.gunjanproject.example.Projectshowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectShowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectShowcaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gunjanproject/example/Projectshowcase/ProjectShowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gunjanproject.example.Projectshowcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectShowcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
